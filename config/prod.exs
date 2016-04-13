3bbc9570dbe516e0f5d03550ac2d6407b44a7d8d
Hector Huertas <hectorhuertas@gmail.com>
2016-04-13 12:01:33 +0200

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,18 @@ use Mix.Config
 # which you typically run after static files are built.
 config :colabora_api, ColaboraApi.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  # url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "colabora-api.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :colabora_api, ColaboraApi.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20,
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -62,4 +72,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
